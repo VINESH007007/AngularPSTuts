de7634d8c2c2004e95a49753819ff53b52f07717
VinuBhai007 <vinesh070707@gmail.com>
2017-08-21 10:11:46 +0530

    Adding it to repo

---
+++ APM-Start/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { ProductService } from './products/product.service';
+import { Component } from '@angular/core';
+@Component({
+  selector : 'pm-root',
+  template : `
+  <div>
+    <nav class='navbar navbar-default'>
+      <div class = 'container-fluid'>
+        <a class = 'navbar-brand'>{{pageTitle}}</a>
+          <ul class = 'nav navbar-nav'> 
+            <li><a [routerLink] = "['/welcome']">Home</a></li>
+            <li><a [routerLink] = "['/products']">Products</a></li>
+          </ul>
+      </div>
+    </nav>
+    <div class='container'>
+      <router-outlet></router-outlet>  
+    </div>
+    
+  </div>
+  `,
+  providers : [ProductService],
+})
+
+export class AppComponent {
+  pageTitle:string = 'Acme Product Management';
+}
